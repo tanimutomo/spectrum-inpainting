07ecbdf379188173a2389ffcaa1080e0f690a496
tanimutomo <tanimu@ht.sfc.keio.ac.jp>
2020-05-27 22:38:50 +0900

    Refactoring Loss

--- src/modules/loss.py
+++ src/modules/loss.py
@@ -8,46 +8,71 @@ from torchvision import models
 def get_loss(cfg):
     cfg = OmegaConf.to_container(cfg, resolve=True)
     return InpaintingLoss(
-        cfg["spec"], cfg["valid"], cfg["hole"], cfg["perc"], cfg["style"],
+        cfg["spec"], cfg["valid"], cfg["hole"], cfg["perc"], cfg["style"], cfg["tv"],
     )
 
 
 class InpaintingLoss(nn.Module):
-    def __init__(self, spec, valid, hole, perc, style):
+    def __init__(self, spec, valid, hole, perc, style, tv):
         super().__init__()
         self.extractor = VGG16FeatureExtractor()
         self.spec_criterion = SpectrumLoss(spec["cut_idx"], spec["norm"], spec["coef"])
         self.valid_criterion = NormLoss(valid["norm"], valid["coef"])
         self.hole_criterion = NormLoss(hole["norm"], hole["coef"])
-        self.perc_criterion = NormLoss(perc["norm"], perc["coef"])
-        self.style_criterion = NormLoss(style["norm"], style["coef"])
+        self.tv_criterion = TotalVariationLoss(tv["coef"])
+        self.perc_style_criterion = PercStyleLoss(
+            perc["norm"], perc["coef"], style["norm"], style["coef"],
+        )
 
     def forward(self, inp, mask, gt, out_img, out_spec, gt_spec) -> dict:
         comp = mask * inp + (1 - mask) * out_img
-
         loss_dict = dict(spec=0.0, valid=0.0, hole=0.0,
                          tv=0.0, perc=0.0, style=0.0)
 
         loss_dict["spec"] = self.spec_criterion(out_spec, gt_spec)
         loss_dict["valid"] = self.valid_criterion(mask * out_img, mask * gt)
         loss_dict["hole"] = self.hole_criterion((1-mask) * out_img, (1-mask) * gt)
-        loss_dict["tv"] = total_variation_loss(comp, mask)
-
-        if self.perc_criterion.coef or self.style_criterion.coef:
-            feats_out = self.extractor(out_img)
-            feats_comp = self.extractor(comp)
-            feats_gt = self.extractor(gt)
-            for i in range(3):
-                if self.perc_criterion.coef:
-                    loss_dict["perc"] += self.perc_criterion(feats_out[i], feats_gt[i])
-                    loss_dict["perc"] += self.perc_criterion(feats_comp[i], feats_gt[i])
-                if self.style_criterion.coef:
-                    loss_dict["style"] += self.style_criterion(gram_matrix(feats_out[i]),
-                                                               gram_matrix(feats_gt[i]))
-                    loss_dict["style"] += self.style_criterion(gram_matrix(feats_comp[i]),
-                                                               gram_matrix(feats_gt[i]))
+        loss_dict["tv"] = self.tv_criterion(comp, mask)
+        loss_dict["perc"], loss_dict["style"] = \
+            self.perc_style_criterion(out_img, comp, gt)
 
         return loss_dict
+
+
+class PercStyleLoss(nn.Module):
+    def __init__(self, perc_norm, perc_coef, style_norm, style_coef):
+        super().__init__()
+        self.extractor = VGG16FeatureExtractor()
+        self.perc_criterion = NormLoss(perc_norm, perc_coef)
+        self.style_criterion = NormLoss(style_norm, style_coef)
+        self.perc, self.style = perc_coef == 0, style_coef == 0
+
+    def forward(self, out, comp, gt):
+        if not self.perc and not self.style:
+            return 0.0, 0.0
+
+        feats_out = self.extractor(out)
+        feats_comp = self.extractor(comp)
+        feats_gt = self.extractor(gt)
+
+        loss_perc, loss_style = 0, 0
+        for i in range(len(feats_gt)):
+            if self.perc:
+                loss_perc += self.perc_criterion(feats_out[i], feats_gt[i])
+                loss_perc += self.perc_criterion(feats_comp[i], feats_gt[i])
+            if self.style:
+                loss_style += self.style_criterion(self.gram_matrix(feats_out[i]),
+                                                   self.gram_matrix(feats_gt[i]))
+                loss_style += self.style_criterion(self.gram_matrix(feats_comp[i]),
+                                                   self.gram_matrix(feats_gt[i]))
+        return loss_perc, loss_style
+
+    def gram_matrix(self, feat):
+        (b, ch, h, w) = feat.size()
+        feat = feat.view(b, ch, h * w)
+        feat_t = feat.transpose(1, 2)
+        gram = torch.bmm(feat, feat_t) / (ch * h * w)
+        return gram
 
 
 # The network of extracting the feature for perceptual and style loss
@@ -129,32 +154,29 @@ class NormLoss(nn.Module):
         return self.criterion(output, target) * self.coef
 
 
-# Calcurate the Gram Matrix of feature maps
-def gram_matrix(feat):
-    (b, ch, h, w) = feat.size()
-    feat = feat.view(b, ch, h * w)
-    feat_t = feat.transpose(1, 2)
-    gram = torch.bmm(feat, feat_t) / (ch * h * w)
-    return gram
+class TotalVariationLoss(nn.Module):
+    def __init__(self, coef):
+        super().__init__()
+        self.coef = coef
 
+    def forward(self, image, mask):
+        if not self.coef: return 0.0
+        hole_mask = 1 - mask
+        dilated_holes = self._dilation_holes(hole_mask)
+        colomns_in_Pset = dilated_holes[:, :, :, 1:] * dilated_holes[:, :, :, :-1]
+        rows_in_Pset = dilated_holes[:, :, 1:, :] * dilated_holes[:, :, :-1:, :]
+        loss = torch.mean(torch.abs(colomns_in_Pset*(
+                    image[:, :, :, 1:] - image[:, :, :, :-1]))) + \
+            torch.mean(torch.abs(rows_in_Pset*(
+                    image[:, :, :1, :] - image[:, :, -1:, :])))
+        return loss
 
-def total_variation_loss(image, mask):
-    hole_mask = 1 - mask
-    dilated_holes = _dilation_holes(hole_mask)
-    colomns_in_Pset = dilated_holes[:, :, :, 1:] * dilated_holes[:, :, :, :-1]
-    rows_in_Pset = dilated_holes[:, :, 1:, :] * dilated_holes[:, :, :-1:, :]
-    loss = torch.mean(torch.abs(colomns_in_Pset*(
-                image[:, :, :, 1:] - image[:, :, :, :-1]))) + \
-           torch.mean(torch.abs(rows_in_Pset*(
-                image[:, :, :1, :] - image[:, :, -1:, :])))
-    return loss
+    def _dilation_holes(self, hole_mask):
+        b, ch, h, w = hole_mask.shape
+        dilation_conv = nn.Conv2d(ch, ch, 3, padding=1, bias=False).to(hole_mask)
+        torch.nn.init.constant_(dilation_conv.weight, 1.0)
+        with torch.no_grad():
+            output_mask = dilation_conv(hole_mask)
+        updated_holes = output_mask != 0
+        return updated_holes.float()
 
-
-def _dilation_holes(hole_mask):
-    b, ch, h, w = hole_mask.shape
-    dilation_conv = nn.Conv2d(ch, ch, 3, padding=1, bias=False).to(hole_mask)
-    torch.nn.init.constant_(dilation_conv.weight, 1.0)
-    with torch.no_grad():
-        output_mask = dilation_conv(hole_mask)
-    updated_holes = output_mask != 0
-    return updated_holes.float()
